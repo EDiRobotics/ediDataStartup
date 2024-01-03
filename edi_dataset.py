3eaec30816047bd405788c8f1e0142f0bfd3ab64
radiance-nt <1808154289@qq.com>
2024-01-03 22:11:04 +0800

    <fix> fix

--- edi_dataset.py
+++ edi_dataset.py
@@ -7,16 +7,19 @@ from data_collection.data import StepLMDBDatasetV2
 Work on pc@192.168.1.240
 """
 
-dataset_directory = "~/ediControlloer/dataset/"
+dataset_directory = "/home/pc/ediController/dataset/"
 dataset_config = "test2.csv"
-
-dataset = StepLMDBDatasetV2(os.path.join(dataset_directory, dataset_config))
+csv_path = os.path.join(dataset_directory, dataset_config)
+print("csv path", csv_path)
+dataset = StepLMDBDatasetV2(csv_path)
 print(len(dataset))
 
 obs, action, instruct = dataset[0]
 print(f"obs: {obs}")
 print(f"action: {action}")
 print(f"instruct: {instruct}")
+
+print("Total Length", len(dataset))
 
 for i, (obs, action, instruct) in enumerate(dataset):
     for item in (obs, action, instruct):
